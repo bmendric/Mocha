04372929092e22ac5ecdbcbd4855feb740763905
QuincyD <quincyd@umich.edu>
2016-10-17 14:00:42 -0400

    ui.py update

--- ui.py
+++ ui.py
@@ -1,56 +1,51 @@
 from Tkinter import *
 from PIL import Image, ImageTk
-
-"""Global variables"""
-xval = 1000
-yval = 500
-
-
-starty = yval*(float(3)/5)
-keyheight = 2*(yval/5) - 10
-keywidth = float(xval)/7 - 10
-
-#startx in pixels
-startx = []
-
-for i in range(7):
-	startx.append(i*keywidth+40)
-	print startx[i]
-
+#from mouseControl import LeapMotionMouse
 
 
 class Application(Frame):
-	
-	def __init__(self, master):
-		""" Initialize application """
-		Frame.__init__(self, master)
-		self.grid()
-		self.startbutton()
+    xval = 1000
+    yval = 500
 
-	def startbutton(self):
-		""" run main menu """
-		self.img = ImageTk.PhotoImage(Image.open("mochaimg.png"))
-		self.mochaButton = Button(root, image = self.img)
-		self.mochaButton.grid(row=0,column=0, padx = ((xval-175)/2), pady = ((yval-75)/2))
-		self.mochaButton["command"] = self.synth
+    starty = yval*(float(3)/5)
+    keywidth = float(xval)/7 - 10
+    keyheight = 2*(yval/5) - 10
 
-	def synth(self):
-		self.mochaButton.grid_forget()
-		self.displaykeys()
-		self.LeapIntoAction()
+    startx = []
+    for i in range(7):
+        startx.append(i*keywidth+40)
 
-	def displaykeys(self):
-		""" display areas for keys """
-		self.space = Canvas(self,width=xval,height=yval)
+    def __init__(self, master):
+        """ Initialize application """
+        Frame.__init__(self, master)
+        self.grid()
+        self.startbutton()
 
-		sy = starty
-		xinc = keywidth # x increment
-		yinc = keyheight # y increment
-		for i in range(7):
-			sx = startx[i]
-			box = [sx,sy,sx+xinc,sy,sx+xinc,sy+yinc,sx,sy+yinc]
-			self.space.create_polygon(box,outline="black",fill="white",width=2)
-			self.space.pack()
+    def startbutton(self):
+        """ run main menu """
+        self.img = ImageTk.PhotoImage(Image.open("mochaimg.png"))
+        self.mochaButton = Button(root, image = self.img)
+        px = (self.xval-175)/2
+        py = (self.yval-75)/2
+        self.mochaButton.grid(row=0,column=0, padx = px, pady = py)
+        self.mochaButton["command"] = self.synth
+
+    def synth(self):
+        self.mochaButton.grid_forget()
+        self.displaykeys()
+
+    def displaykeys(self):
+        """ display areas for keys """
+        self.space = Canvas(self,width=self.xval,height=self.yval)
+    
+        sy = self.starty
+        xinc = self.keywidth # x increment
+        yinc = self.keyheight # y increment
+        for i in range(7):
+            sx = self.startx[i]
+            box = [sx,sy,sx+xinc,sy+yinc]
+            self.space.create_rectangle(box,outline="black",fill="white",width=2)
+            self.space.pack()
 
 	def LeapIntoAction(self):
 		""" run leap motion synthesizer """
@@ -71,7 +66,7 @@ class Application(Frame):
 root = Tk()
 root.title("Mocha")
 
-root.geometry(str(xval)+"x"+str(yval))
+root.geometry("1000x500")
 app = Application(root)
 
 root.mainloop()
